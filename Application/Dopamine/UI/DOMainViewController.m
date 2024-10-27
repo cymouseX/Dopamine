c523e906bc7fa9ab6ff9be8b65ded73a291091ad
cypwn <40589571+cymouseX@users.noreply.github.com>
2024-10-27 19:11:45 +0700

    Update DOMainViewController.m

--- Application/Dopamine/UI/DOMainViewController.m
+++ Application/Dopamine/UI/DOMainViewController.m
@@ -51,7 +51,7 @@
 
                             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3.0 * NSEC_PER_SEC), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
             dispatch_async(dispatch_get_main_queue(), ^{
-    exit(0); 
+    //exit(0); 
             });   });
             
 
